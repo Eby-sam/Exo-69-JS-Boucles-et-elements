0feb245fc8a98b27eda90c81a2dfdefb3a06bf53
coque <coquelet.samuel@gmail.com>
2021-11-17 16:09:07 +0100

    fini

--- exo 2/app.js
+++ exo 2/app.js
@@ -2,3 +2,12 @@ let liste = document.getElementById('liste-commissions');
 let beure = document.createElement("li");
 liste.appendChild(beure);
 beure.innerHTML = 'Beure';
+
+// button ...
+
+let button = document.getElementById('touch');
+function reset() {
+    beure.remove();
+}
+
+button.addEventListener('click',reset);
